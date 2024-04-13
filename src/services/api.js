1a1403b10cdecf04bf11b0a80d66028978a2e68b
github-usman <usmantoyou@gmail.com>
2024-04-13 19:32:21 +0530

    stablished Fake server to avoid rate limit of original server

--- src/services/api.js
+++ src/services/api.js
@@ -5,15 +5,16 @@ const BASE_URL = process.env.REACT_APP_BASE_API_URL;
 
 const fetchTopCoinsData = async (vsCurrency = 'inr', perPage = 10, page = 1) => {
   try {
-    const response = await axios.get(`${BASE_URL}/coins/markets`, {
-      params: {
-        vs_currency: vsCurrency,
-        order: 'market_cap_desc',
-        per_page: perPage,
-        page: page,
-        sparkline: false,
-        price_change_percentage: '24h,7d',
-      },
+    // const response = await axios.get(`${BASE_URL}/coins/markets`, {
+    const response = await axios.get(`${BASE_URL}/data`, {
+      // params: {
+      //   vs_currency: vsCurrency,
+      //   order: 'market_cap_desc',
+      //   per_page: perPage,
+      //   page: page,
+      //   sparkline: false,
+      //   price_change_percentage: '24h,7d',
+      // },
     });
     console.log('api call test',response.data);
     return response.data;
