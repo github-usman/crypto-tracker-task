d9142c0652f2900245d1160c5efde260fde35961
github-usman <usmantoyou@gmail.com>
2024-04-26 10:16:45 +0530

    hook test

--- src/components/crypto-currencies/coin-list/CoinList.jsx
+++ src/components/crypto-currencies/coin-list/CoinList.jsx
@@ -15,6 +15,7 @@ const CoinList = () => {
     const fetchData = async () => {
       try {
         const data = await fetchTopCoinsData('inr', 10, currentPage);
+        // console.log('jsdfdsf')
         setCoinData(data);
         setLoading(false);
       } catch (error) {
